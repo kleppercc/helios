5dedbc789e236ba6ec2c8a1eac3e3fcfbae2b04e
Ezekial Unterberg <unterberge@fusion.gat.com>
2014-01-09 07:57:10 -0800

    Fixed up to pass the pandas DataFrames.name all the way through.

--- Python/SmoothUtils.py
+++ Python/SmoothUtils.py
@@ -2,28 +2,30 @@
 import numpy as np
 import pandas as pds
 
-def Takeoff_bac(wav,tubeNum,pntsm,interppnt,frac=50,quiet=True):
+def Takeoff_bac(wav,pntsm,interppnt,frac,quiet=True):
 	if not quiet:
-		print 'here'
-		wavNum = wav.shape[1]
-		for i in xrange(wavNum):
-			print wav[i].name
-	wav2 = Get_back(wav,tubeNum,pntsm,frac)
+		print 'Takeoff_bac t1: {}'.format(wav.name)
+	wav2 = Get_back(wav,pntsm,frac)
 	wav4 = pds.stats.moments.rolling_min(wav2,interppnt)
-	# wav4 = PerctSmooth(wav2,wav2.index[0],wav2.index[-1],frac=10)
+	wav4 = wav4.fillna(0)
 	OutWav = wav - wav4
+	OutWav = wav.sub(wav4)
+	if not quiet:
+		print 'Takeoff_bac t2: {}'.format(OutWav)
+		print 'Takeoff_bac t3: {}'.format(wav)
+		print 'Takeoff_bac t4: {}'.format(wav4)
 	return OutWav
 
-def Get_back(wav,tubeNum,pntsm,frac):
+def Get_back(wav,pntsm,frac):
 	wavpnts = len(wav)
-	for i in xrange(tubeNum):
-		if i == 0:
-			wav2 = pds.DataFrame(wav[i],index=wav[i].index)
-			wav2.columns = [i]
-		else:
-			wav2[i] = wav[i]
-		print wav[i].name
-		wav2[i].name = wav[i].name+'_bac'
+	## == old way using DataFrames, not needed, but nice technique, so saved
+	# listholder =[]
+	# for i in xrange(tubeNum):
+		# print wav[i].name+'_bac'
+		# listholder.append(pds.Series(wav[i]),name = wav[i].name+'_bac')
+	# wav2 = pds.DataFrame(listholder)
+	## =====
+	wav2 = pds.Series(wav)
 	for i in xrange(0,wavpnts,pntsm):
 		result = PerctSmooth(wav,i,(i+pntsm),frac)
 		wav2[i:(i+pntsm)] = result.value
