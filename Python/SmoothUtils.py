f6f570b887cc1fa3208af0edc37a657a15cca058
Ezekial Unterberg <unterberge@fusion.gat.com>
2014-01-09 07:57:10 -0800

    Fixed up to pass the pandas DataFrames.name all the way through.

--- Python/SmoothUtils.py
+++ Python/SmoothUtils.py
@@ -8,13 +8,23 @@ def Takeoff_bac(wav,pntsm,interppnt,frac,quiet=True):
 	wav2 = Get_back(wav,pntsm,frac)
 	wav4 = pds.stats.moments.rolling_min(wav2,interppnt)
 	wav4 = wav4.fillna(0)
+	OutWav = wav - wav4
 	OutWav = wav.sub(wav4)
 	if not quiet:
 		print 'Takeoff_bac t2: {}'.format(OutWav)
+		print 'Takeoff_bac t3: {}'.format(wav)
+		print 'Takeoff_bac t4: {}'.format(wav4)
 	return OutWav
 
 def Get_back(wav,pntsm,frac):
 	wavpnts = len(wav)
+	## == old way using DataFrames, not needed, but nice technique, so saved
+	# listholder =[]
+	# for i in xrange(tubeNum):
+		# print wav[i].name+'_bac'
+		# listholder.append(pds.Series(wav[i]),name = wav[i].name+'_bac')
+	# wav2 = pds.DataFrame(listholder)
+	## =====
 	wav2 = pds.Series(wav)
 	for i in xrange(0,wavpnts,pntsm):
 		result = PerctSmooth(wav,i,(i+pntsm),frac)
