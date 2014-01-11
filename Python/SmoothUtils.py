88e17572af6d79b3a75001c422011cf912ea5ba1
Ezekial Unterberg <unterberge@fusion.gat.com>
2014-01-10 18:26:57 -0800

    removed legacy code comments.

--- Python/SmoothUtils.py
+++ Python/SmoothUtils.py
@@ -15,13 +15,6 @@ def Takeoff_bac(wav,pntsm,interppnt,frac,quiet=True):
 
 def Get_back(wav,pntsm,frac):
 	wavpnts = len(wav)
-	## == old way using DataFrames, not needed, but nice technique, so saved
-	# listholder =[]
-	# for i in xrange(tubeNum):
-		# print wav[i].name+'_bac'
-		# listholder.append(pds.Series(wav[i]),name = wav[i].name+'_bac')
-	# wav2 = pds.DataFrame(listholder)
-	## =====
 	wav2 = pds.Series(wav)
 	for i in xrange(0,wavpnts,pntsm):
 		result = PerctSmooth(wav,i,(i+pntsm),frac)
