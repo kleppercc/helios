8874709227df3ac7b0f2d6679ac66786e8a137b0
Ezekial Unterberg <unterberge@fusion.gat.com>
2014-01-10 16:52:57 -0800

    cleaned up.

--- Python/getSHOT.py
+++ Python/getSHOT.py
@@ -1,10 +1,9 @@
 import h5py
 import scipy.io as sio
 
-
 def getShotmat(shot=118800,fnam=None):
 
-    if fnam==None:
+    if (fnam==None):
         fnam = 'shot_'+str(shot)
     hold = sio.loadmat(fnam,appendmat=True,squeeze_me=True,struct_as_record=True)
 
@@ -26,19 +25,25 @@ def getShotmat(shot=118800,fnam=None):
     she =hold['data']['she1']
 
     fstore = h5py.File('s'+str(shot)+'_wavs.h5','w')
-    for i in xrange(0,len(sigs)):
+    print len(sigs)
+    for i in xrange(len(sigs)):
         ds = fstore.create_dataset('y_'+sigs[i],hold['data'][sigs[i]][()]['y'][()].shape,'f',maxshape=(None))
         ds.write_direct(hold['data'][sigs[i]][()]['y'][()])
         ds = fstore.create_dataset('x_'+sigs[i],hold['data'][sigs[i]][()]['x0'][()].shape,'f',maxshape=(None))
         ds.write_direct(hold['data'][sigs[i]][()]['x0'][()])
     fstore.close()
 
-    return 'Complete download: '+str(shot)
+    message = 'Complete download: '+str(shot)
+    return message
 
-def load2DICT(fNAM):
+def load2DICT(fNAM=None):
+    if fNAM == None:
+        print 'yeah'
+    
     filIN = h5py.File(fNAM,'r')
     Names = filIN.keys()
     outDICT = {}
-    for i in len(Names):
+    for i in xrange(len(Names)):
         outDICT[Names[i]] = filIN[Names[i]].value
+    
     return outDICT
